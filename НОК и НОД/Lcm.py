f9add154d8fc7f3e84191c0dd2a6485fc2c965b3
Z1at <nikandrov2103@gmail.com>
2025-01-10 18:39:21 +0300

    sorting, gcd, lcm

---
+++ НОК и НОД/Lcm.py
@@ -0,0 +1,7 @@
+import math
+print(math.lcm(*map(int, input().split())))
+
+
+"""
+НОК(а, b) = a * b / НОД(a, b)
+"""
---
+++ Сортировки/Bubble_sort.py
@@ -0,0 +1,38 @@
+"""
+Сортировка пузырьком
+Bubble Sort algorithm, which sorts an array by repeatedly comparing adjacent elements and swapping them if they are in
+the wrong order. The algorithm iterates through the array multiple times, with each pass pushing the largest unsorted
+element to its correct position at the end.
+"""
+
+
+def bubble_sort(arr):
+    # Outer loop to iterate through the list n times
+    for n in range(len(arr) - 1, 0, -1):
+
+        # Initialize swapped to track if any swaps occur
+        swapped = False
+
+        # Inner loop to compare adjacent elements
+        for i in range(n):
+            if arr[i] > arr[i + 1]:
+                # Swap elements if they are in the wrong order
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+                # Mark that a swap has occurred
+                swapped = True
+
+        # If no swaps occurred, the list is already sorted
+        if not swapped:
+            break
+
+
+# Sample list to be sorted
+arr = [39, 12, 18, 85, 72, 10, 2, 18]
+print("Unsorted list is:")
+print(arr)
+
+bubble_sort(arr)
+
+print("Sorted list is:")
+print(arr)
